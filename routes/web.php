c73ee7f4372baa4ff8d69e6456b55e9d3aad27f0
alihasana <hasana.ali@gmail.com>
2017-12-12 17:06:55 +0100

    initial checkin

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello', function () {
+    return view('hello/hello');
+});
+
+
+Route::get('/', 'welcomeController@index');
+
+Route::get('/ali', function () {
+    return '<h1>This is alihasana</h1>';
+});
+
+Route::get('/about', 'welcomeController@ali');
+
+Route::get('image-gallery', 'ImageGalleryController@index');
+Route::post('image-gallery', 'ImageGalleryController@upload');
+Route::delete('image-gallery/{id}', 'ImageGalleryController@destroy');
